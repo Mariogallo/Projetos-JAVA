29ae924f47e618b96eaab913f7e388a5ac442cd4
Mariogallo <prof.eng.mariogallo@gmail.com>
2021-07-26 12:15:42 -0300

    Estou publicando meus exercicios de Java Completo

---
+++ hashcode/src/application/Program2.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		String a = "Maria";
+		String b = "Maria";
+		
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+
+	}
+
+}
